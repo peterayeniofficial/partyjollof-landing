d8abcaf1b538c15cb97add2533f8354c8598d401
Peter Ayeni <ayenicology@gmail.com>
2023-12-27 14:02:34 +0000

    update metadata

---
+++ src/app/config/siteConfig.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+    name: "Party Jollof",
+    url: "https://www.partyjollof.org/",
+    ogImage: "https://www.partyjollof.org/og.png",
+    description:
+      "Discover Party Jollof, the premier destination for African movies and TV shows reviews. Explore critic and audience insights, latest news, trailers, and ratings. Find comprehensive box office details, browse by genre or platform, and stay updated with trending topics in African cinema. Whether your are seeking popular hits or upcoming gems, Party Jollof is your one-stop source for all things African entertainment.",
+    links: {
+      twitter: "#",
+      github: "#",
+    },
+    title: 'Party Jollof - Your Ultimate Guide to African Movies & TV Shows Reviews and Ratings',
+  }
+  
+  export type SiteConfig = typeof siteConfig
