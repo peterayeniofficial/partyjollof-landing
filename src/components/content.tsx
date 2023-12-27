3b7fd877abf51b81878816e7c40b9dea7f351e69
Peter Ayeni <ayenicology@gmail.com>
2023-12-27 20:24:21 +0000

    user can play trailer

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -2,6 +2,8 @@ import Image from "next/image"
 
 import { cn } from "@/lib/utils"
 import { ContentData } from "../../data/content"
+import { PlayCircle } from "lucide-react"
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog"
 
 
 
@@ -21,8 +23,10 @@ export function Content({
   ...props
 }: ContentProps) {
   return (
-    <div className={cn("space-y-3", className)} {...props}>
-      
+    <div className={cn("space-y-3 relative group", className)} {...props}>
+
+      <Dialog>
+        <DialogTrigger asChild>
           <div className="overflow-hidden rounded-md">
             <Image
               src={album.cover}
@@ -30,12 +34,37 @@ export function Content({
               width={width}
               height={height}
               className={cn(
-                "h-auto w-auto object-cover transition-all hover:scale-105",
+                "h-auto w-auto object-cover transition-all hover:scale-105 cursor-pointer",
                 aspectRatio === "portrait" ? "aspect-[3/4]" : "aspect-square"
               )}
             />
+            <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity'>
+
+              <PlayCircle size={50} color="#ffffff" absoluteStrokeWidth />
+            </div>
           </div>
-       
+        </DialogTrigger>
+        <DialogContent className="w-[750px] h-auto">
+          <DialogHeader>
+            <DialogTitle className="text-white">{album.name} Trailer</DialogTitle>
+            
+          </DialogHeader>
+          <div className="flex items-center">
+
+            <iframe
+              allow="autoplay; encrypted-media"
+              className="w-full border-none"
+              src={album.trailer}
+              allowFullScreen={true}
+            />
+
+          </div>
+          <DialogFooter className="sm:justify-start">
+            <p>watch</p>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+
       <div className="space-y-1 text-sm">
         <h3 className="font-medium text-white leading-none">{album.name}</h3>
         <p className="text-xs text-muted-foreground">{album.artist}</p>
